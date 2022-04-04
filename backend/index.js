81761946ac5292bb895c629ce784a17f2292a360
FilipNR <rfilip5@protonmail.com>
2022-04-04 09:19:18 +0200

    Patched

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const schedule = require('node-schedule')
 const { Scrape } = require('./parse')
-const port = 8080
+const port = 443 
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.sendFile('radovi.json', { root: __dirname })
 })
 
